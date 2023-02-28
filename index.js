8c81ee02c15c2e1e4b9553e31673fbf14b7d24b2
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-02-28 20:36:45 +0000

    Add comments

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Code to gather information about the development team members, and render the HTML file.
 
 const team = [];
 
+// Questions to get the manager's info
 inquirer
   .prompt([
     {
@@ -22,6 +23,7 @@ inquirer
       message: "What is your team manager's name?",
       name: "name",
       validate(input) {
+        // If the user's input is empty it prompts the user to add a name.
         if (input === "") {
           return "Please add the manager's name!";
         }
@@ -33,6 +35,7 @@ inquirer
       message: "What is your team manager's id?",
       name: "id",
       validate(input) {
+        // If the user's input is empty it prompts the user to add a ID.
         if (input === "") {
           return "Please add the manager's ID!";
         }
@@ -44,6 +47,7 @@ inquirer
       message: "What is your team manager's email?",
       name: "email",
       validate: (input) => {
+        // It returns true if it's a valid input, otherwise it prompts the user to enter a valid email address.
         return emailValidator.validate(input)
           ? true
           : "Please enter a valid email address";
@@ -54,6 +58,7 @@ inquirer
       message: "What is your team manager's office number?",
       name: "officeNumber",
       validate(input) {
+        // If the user's input is empty it prompts the user to add an office number.
         if (input === "") {
           return "Please add the manager's office number";
         }
@@ -62,7 +67,9 @@ inquirer
     },
   ])
   .then((res) => {
+    // Using manager class and info the user enters to create a manager object.
     const manager = new Manager(res.name, res.id, res.email, res.officeNumber);
+    // Add the manager object to the team array.
     team.push(manager);
     nextEmployeePrompt();
   });
@@ -82,17 +89,20 @@ const nextEmployeePrompt = () => {
       },
     ])
     .then((res) => {
+      // Conditional if to choose which prompt will be called depending on the choice the user makes.
       if (res.nextEmployee === "Engineer") {
         promptForEngineer();
       } else if (res.nextEmployee === "Intern") {
         promptForIntern();
       } else {
+        // Calls function to generate html and write on team.html
         buildPage();
       }
     });
 };
 
 const promptForEngineer = () => {
+  // Questions to get the engineer's info
   inquirer
     .prompt([
       {
@@ -100,6 +110,7 @@ const promptForEngineer = () => {
         message: "What is your engineer's name?",
         name: "name",
         validate(input) {
+            // If the user's input is empty it prompts the user to add a name.
             if (input === "") {
               return "Please add the engineer's name!";
             }
@@ -111,6 +122,7 @@ const promptForEngineer = () => {
         message: "What is your engineer's id?",
         name: "id",
         validate(input) {
+            // If the user's input is empty it prompts the user to add a ID.
             if (input === "") {
               return "Please add the engineer's ID!";
             }
@@ -122,6 +134,7 @@ const promptForEngineer = () => {
         message: "What is your engineer's email?",
         name: "email",
         validate: (input) => {
+          // It returns true if it's a valid input, otherwise it prompts the user to enter a valid email address.
           return emailValidator.validate(input)
             ? true
             : "Please enter a valid email address";
@@ -132,6 +145,7 @@ const promptForEngineer = () => {
         message: "What is your engineer's GitHub username?",
         name: "github",
         validate(input) {
+            // If the user's input is empty it prompts the user to add a Github username.
             if (input === "") {
               return "Please add the engineer's GitHub username!";
             }
@@ -140,13 +154,16 @@ const promptForEngineer = () => {
       },
     ])
     .then((res) => {
+      // Using engineer class and info the user enters to create an engineer object.
       const engineer = new Engineer(res.name, res.id, res.email, res.github);
+      // Add the engineer object to team array.
       team.push(engineer);
       nextEmployeePrompt();
     });
 };
 
 const promptForIntern = () => {
+  // Questions to get the intern's info
   inquirer
     .prompt([
       {
@@ -154,6 +171,7 @@ const promptForIntern = () => {
         message: "What is your intern's name?",
         name: "name",
         validate(input) {
+            // If the user's input's is empty it prompts the user to add a name.
             if (input === "") {
               return "Please add the intern's name!";
             }
@@ -165,6 +183,7 @@ const promptForIntern = () => {
         message: "What is your intern's id?",
         name: "id",
         validate(input) {
+            // If the user's input's is empty it prompts the user to add a ID.
             if (input === "") {
               return "Please add the intern's ID!";
             }
@@ -176,6 +195,7 @@ const promptForIntern = () => {
         message: "What is your intern's email?",
         name: "email",
         validate: (input) => {
+          // It returns true if it's a valid input, otherwise it prompts the user to enter a valid email address.
           return emailValidator.validate(input)
             ? true
             : "Please enter a valid email address";
@@ -186,6 +206,7 @@ const promptForIntern = () => {
         message: "What is your intern's school?",
         name: "school",
         validate(input) {
+            // If the user's input's is empty it prompts the user to add a school name.
             if (input === "") {
               return "Please add the intern's school name!";
             }
@@ -194,12 +215,15 @@ const promptForIntern = () => {
       },
     ])
     .then((res) => {
+      // Using intern class and info the user enters to create an intern object.
       const intern = new Intern(res.name, res.id, res.email, res.school);
+      // Add intern object to team array.
       team.push(intern);
       nextEmployeePrompt();
     });
 };
 
+// Function to render hmtl in team.html file.
 const buildPage = () => {
   fs.writeFileSync(outputPath, render(team));
   console.log("Your team profile html page has been created successfully!");
